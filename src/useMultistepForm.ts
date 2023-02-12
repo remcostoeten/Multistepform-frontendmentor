16fd53c3da964ea6810b6dc56f13aca18a3d6caf
remcostoeten <remcostoeten@hotmail.com>
2023-02-12 16:57:08 +0100

    Setup basic function

---
+++ src/useMultistepForm.ts
@@ -0,0 +1,33 @@
+import { ReactElement } from 'React';
+export function useMultistepForm(steps: ReactElement[]) {
+	const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+	function next() {
+		setCurrentStepIndex((i) => {
+			if (i >= steps.length - 1) return i;
+			return i + 1;
+		});
+	}
+
+	function back() {
+		setCurrentStepIndex((i) => {
+			if (i <= 0) return i;
+			return i - 1;
+		});
+	}
+
+	function goTo(index: number) {
+		setCurrentStepIndex(index);
+	}
+
+	return {
+		currentStepIndex,
+		step: steps[currentStepIndex],
+		goTo,
+		next,
+		back,
+	};
+}
+function useState(arg0: number): [any, any] {
+	throw new Error('Function not implemented.');
+}
