1f922e62aefce494e7bf5b8aee6f988183417951
remcostoeten <remcostoeten@hotmail.com>
2023-02-12 23:31:29 +0100

    somewhat working step logic.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,14 @@ import { useMultistepForm } from '../useMultistepForm';
 
 function Footer() {
 	const steps = 1;
-	// const { currentStepIndex } = useMultistepForm([]);
-	const { currentStepIndex } = 1;
+	const { currentStepIndex } = useMultistepForm([]);
 
 	return (
 		<div className='footer'>
 			<form className='footer__form'>
-				<div>{currentStepIndex}</div>
+				<div>
+					{currentStepIndex} / {steps}
+				</div>
 			</form>
 		</div>
 	);
--- src/useMultistepForm copy.tsx
+++ src/useMultistepForm copy.tsx
@@ -4,14 +4,14 @@ export function useMultistepForm(steps: ReactElement[]) {
 	const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
 	function next() {
-		setCurrentStepIndex((i: number) => {
+		setCurrentStepIndex((i) => {
 			if (i >= steps.length - 1) return i;
 			return i + 1;
 		});
 	}
 
 	function back() {
-		setCurrentStepIndex((i: number) => {
+		setCurrentStepIndex((i) => {
 			if (i <= 0) return i;
 			return i - 1;
 		});
@@ -23,7 +23,10 @@ export function useMultistepForm(steps: ReactElement[]) {
 
 	return {
 		currentStepIndex,
+		step: steps[currentStepIndex],
 		steps,
+		isFirstStep: currentStepIndex === 0,
+		isLastStep: currentStepIndex === steps.length - 1,
 		goTo,
 		next,
 		back,
