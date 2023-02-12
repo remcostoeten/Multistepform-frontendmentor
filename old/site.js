d252ccadbc8a1ad1727f4ef6b8b1a93adadc36be
remcostoeten <remcostoeten@hotmail.com>
2023-02-12 16:33:05 +0100

    Setup react app with tsx/vite

---
+++ old/site.js
@@ -0,0 +1,10 @@
+const stepOne = document.getElementsByClassName('first-step');
+const hideBack = document.getElementsByClassName('btn--back');
+for (const i = 0; i < hideBack.length; i++) {
+	hideBack.classList.add('aa');
+}
+
+for (const i = 0; i < hideBack.length; i++) {
+	console.log(hideBack);
+}
+alert('test');
--- site.js
+++
@@ -1,5 +0,0 @@
-const stepOne = document.getElementsByClassName('first-step');
-const hideBack = document.getElementById('back');
-if (stepOne.length === 1) {
-	hideBack.classList.add('unread');
-}
