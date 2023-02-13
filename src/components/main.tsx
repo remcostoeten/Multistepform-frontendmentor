ab773babcb0a441e59787d62ed20cc57a64f8ea8
remcostoeten <remcostoeten@hotmail.com>
2023-02-13 15:53:13 +0100

    Mobile styling

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -5,9 +5,14 @@ export default function Main() {
 	return (
 		<>
 			<main className='info'>
-				<h1>Personal Info</h1>
-				<p>Please provide your name, email address and phone number.</p>
-				<StepOneForm />
+				<div className='info__inner'>
+					<h1>Personal Info</h1>
+					<p>
+						Please provide your name, email address and phone
+						number.
+					</p>
+					<StepOneForm />
+				</div>
 				<Footer />
 			</main>
 		</>
